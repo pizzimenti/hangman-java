9dcbf99971599969cc3507c1ed19cd5b0f9b7817
Bradley Pizzimenti <bradley@gennakersystems.com>
2019-01-20 17:41:55 -0900

    add hit guess checking

--- Game.java
+++ Game.java
@@ -11,11 +11,23 @@ class Game {
 
     public boolean applyGuess(char letter) {
         boolean isHit = answer.indexOf(letter) != -1;
-        if (isHit){
+        if (isHit) {
             hits += letter;
-        }else{
+        } else {
             misses += letter;
         }
         return isHit;
+    }
+
+    public String getCurrentProgress() {
+        String progress = "";
+        for (char letter : answer.toCharArray()) {
+            char display = '-';
+            if (hits.indexOf(letter) != -1) {
+                display = letter;
+            }
+            progress += display;
+        }
+        return progress;
     }
 }
--- Hangman.java
+++ Hangman.java
@@ -1,5 +1,14 @@
 public class Hangman {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Game game = new Game("treehouse");
+        Prompter prompter = new Prompter(game);
+        prompter.displayProgress();
+        boolean isHit = prompter.promptForGuess();
+        if (isHit) {
+            System.out.println("We got a hit!");
+        } else {
+            System.out.println("Oops, missed");
+        }
+        prompter.displayProgress();
     }
 }
